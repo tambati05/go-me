4f30f404ad770594df0f185b8f24ede6ac9d7b0d
tambati05 <tarunambati05@gmail.com>
2025-02-26 11:51:41 -0600

    Create main.go inside array folder

---
+++ array/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare an array with 5 elements
+	var arr [5]int
+
+	// Assign values to the array
+	arr[0] = 10
+	arr[1] = 20
+	arr[2] = 30
+	arr[3] = 40
+	arr[4] = 50
+
+	// Print the array
+	fmt.Println(arr)
+}
